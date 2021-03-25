4c817c158a6ed52f3237846db29dc624ff8e7857
georgebelll <georgebell.com@gmail.com>
2021-03-25 10:38:44 +0000

    Initial project files pushed to nucode

---
+++ Assets/Fellow.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fellow : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0.05f;
+
+    int score = 0;
+    int pelletsEaten = 0;
+    [SerializeField]
+    int pointsPerPellet = 100;
+
+    [SerializeField]
+    float powerupDuration = 10.0f;
+
+    float powerupTime = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.z += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.z -= speed;
+        }
+        transform.position = pos;
+
+        powerupTime = Mathf.Max(0.0f, powerupTime - Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody b = GetComponent<Rigidbody>();
+        Vector3 velocity = b.velocity;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.z = speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.z = -speed;
+        }
+        b.velocity = velocity;
+     }
+
+    [SerializeField]
+    BoxCollider LeftTeleporter;
+
+    [SerializeField]
+    BoxCollider RightTeleporter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 pos = transform.position;
+
+        if (other.gameObject.CompareTag("Pellet"))
+        {
+            pelletsEaten++;
+            score += pointsPerPellet;
+            Debug.Log("Score is: " + score);
+        }
+
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            powerupTime = powerupDuration;
+        }
+
+        if (other.gameObject.CompareTag("LeftTeleporter"))
+        {
+            pos = RightTeleporter.ClosestPoint(pos);
+            Debug.Log("LeftTeleporter found");
+            pos.x = pos.x - 0.5f;
+            transform.position = pos;
+        }
+
+        if (other.gameObject.CompareTag("RightTeleporter"))
+        {
+            pos = LeftTeleporter.ClosestPoint(pos);
+            Debug.Log("RightTeleporter found");
+            pos.x = pos.x + 0.5f;
+            transform.position = pos;
+        }
+
+    }
+    public bool PowerupActive()
+    {
+        return powerupTime > 0.0f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ghost"))
+        {
+            Debug.Log("You Died with a score of " + score);
+            gameObject.SetActive(false);
+        }
+    }
+    public int PelletsEaten()
+    {
+        return pelletsEaten;
+    }
+
+    
+
+}
---
+++ Assets/Ghost.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ghost : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+
+
+    [SerializeField]
+    Material scaredMaterial;
+    Material normalMaterial;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = PickRandomPosition();
+        normalMaterial = GetComponent<Renderer>().material;
+    }
+
+    bool hiding = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.PowerupActive())
+        {
+            Debug.Log("Hiding from Player!");
+            if(!hiding || agent.remainingDistance < 0.5f)
+            {
+                hiding = true;
+                agent.destination = PickHidingPlace();
+                GetComponent<Renderer>().material = scaredMaterial;
+            }
+            
+        }
+        else
+        {
+           // Debug.Log("Chasing Player!");
+            if (hiding)
+            {
+                GetComponent<Renderer>().material = normalMaterial;
+            }
+
+            if (agent.remainingDistance < 0.5f)
+            {
+                agent.destination = PickRandomPosition();
+                hiding = false;
+                GetComponent<Renderer>().material = normalMaterial;
+            }
+           
+        }
+
+
+        if (CanSeePlayer())
+        {
+            Debug.Log("I can see you!");
+            agent.destination = player.transform.position;
+        }
+        else
+        {
+            if (agent.remainingDistance < 0.5f)
+            {
+                agent.destination = PickRandomPosition();
+            }
+
+        }
+
+    }
+
+    Vector3 PickRandomPosition()
+    {
+        Vector3 destination = transform.position;
+        Vector2 randomDirection = UnityEngine.Random.insideUnitCircle * 8.0f;
+        destination.x += randomDirection.x;
+        destination.y += randomDirection.y;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(destination, out navHit, 8.0f, NavMesh.AllAreas);
+
+        return navHit.position;
+    }
+
+    [SerializeField]
+    Fellow player;
+
+    bool CanSeePlayer()
+    {
+        Vector3 rayPos = transform.position;
+        Vector3 rayDir = (player.transform.position - rayPos).normalized;
+
+        RaycastHit info;
+        if (Physics.Raycast(rayPos, rayDir, out info))
+        {
+            if (info.transform.CompareTag("Fellow"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    Vector3 PickHidingPlace()
+    {
+        Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(transform.position - (directionToPlayer * 8.0f), out navHit, 8.0f, NavMesh.AllAreas);
+
+        return navHit.position;
+    }
+
+
+    [SerializeField]
+    BoxCollider LeftTeleporter;
+
+    [SerializeField]
+    BoxCollider RightTeleporter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 pos = transform.position;
+
+
+        if (other.gameObject.CompareTag("LeftTeleporter"))
+        {
+            pos = RightTeleporter.ClosestPoint(pos);
+            Debug.Log("LeftTeleporter found");
+            pos.x = pos.x - 0.5f;
+            transform.position = pos;
+        }
+
+        if (other.gameObject.CompareTag("RightTeleporter"))
+        {
+            pos = LeftTeleporter.center;
+            Debug.Log("RightTeleporter found");
+            pos.x = pos.x + 0.5f;
+            transform.position = pos;
+        }
+
+    }
+
+
+
+
+}
